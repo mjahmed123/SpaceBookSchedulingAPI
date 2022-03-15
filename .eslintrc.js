b45cbae02192158e8f77efd30be50e9def40de8a
mjahmed123 <94318179+mjahmed123@users.noreply.github.com>
2022-03-15 18:34:58 +0000

    Installed ESlint, axios and scheduler API

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  env: {
+    commonjs: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  rules: {
+    'linebreak-style': 0,
+    'no-console': 'off',
+  },
+};
